a9de8255200e0ff618b802140b86f8e38b85447a
DESKTOP-K72OSB8\beomukim <gld55602@gmail.com>
2021-09-27 20:29:30 +0900

    test

--- src/main/java/com/memo/test/TestController.java
+++ src/main/java/com/memo/test/TestController.java
@@ -1,17 +1,32 @@
 package com.memo.test;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+import com.memo.test.bo.TestBO;
+
 @Controller
 public class TestController {
-	  @ResponseBody
-	    @RequestMapping("/test1")
-	    public String helloWorld() {
-	        return "Hello spring!";
-	    }
+	
+	@Autowired
+	private TestBO testbo;
+	@ResponseBody
+	@RequestMapping("/test1")
+	public String helloWorld() {
+		return "Hello spring!";
+	}
+	
+	@RequestMapping("/test2")
+	@ResponseBody
+	public int test() {
+		return testbo.test();
+	}
+	
+	@RequestMapping("/test3")
+	public String test3() {
+		return "test/test";
+	}
+
 }
---
+++ src/main/java/com/memo/test/dao/TestDAO.java
@@ -0,0 +1,8 @@
+package com.memo.test.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TestDAO {
+	public int getId();
+}
